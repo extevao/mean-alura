8a4b983635a26f8d435f46527f672eab4830d2a5
Estevao <estevaovasques@hotmail.com>
2017-02-02 20:59:38 -0200

    Criado o primeiro get, retornando uma lista estática

--- config/express.js
+++ config/express.js
@@ -3,4 +3,19 @@ var app = express();
 
 app.use(express.static('./public'));
 
+app.get('/v1/fotos', function(req, res) {
+    var fotos = [{
+            _id: 1,
+            titulo: 'Leão',
+            url: 'http://www.fundosanimais.com/Imagens/fotos-leoes.jpg'
+        },
+        {
+            _id: 2,
+            titulo: 'Leão2',
+            url: 'http://www.fundosanimais.com/Imagens/fotos-leoes.jpg'
+        }
+    ];
+    res.json(fotos);
+});
+
 module.exports = app;
