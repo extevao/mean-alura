5147036db3ea95eefdf2f7c5a2db8ee3ea543a56
Estevao <estevaovasques@hotmail.com>
2017-02-05 15:47:19 -0200

    Alterado para passar por parametro a uri do banco para a configuração do mongodb

--- config/database.js
+++ config/database.js
@@ -1,23 +1,26 @@
 var mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost/alurapic');
+module.exports = function(uri) {
 
-mongoose.connection.on('connected', function() {
-    console.log('Conectado ao MongoDB');
-});
+    mongoose.connect('mongodb://' + uri);
 
-mongoose.connection.on('error', function(error) {
-    console.log('Erro na conexão com o MongoDB. \n ' + error);
-});
-
-mongoose.connection.on('disconnected', function() {
-    console.log('Desconectado do MongoDB');
-});
-
-process.on('SIGINT', function() {
-    mongoose.connection.close(function() {
-        console.log('Conexão fechada pelo témino da aplicação');
-        process.exit(0);
+    mongoose.connection.on('connected', function() {
+        console.log('Conectado ao MongoDB');
     });
-});
+
+    mongoose.connection.on('error', function(error) {
+        console.log('Erro na conexão com o MongoDB. \n ' + error);
+    });
+
+    mongoose.connection.on('disconnected', function() {
+        console.log('Desconectado do MongoDB');
+    });
+
+    process.on('SIGINT', function() {
+        mongoose.connection.close(function() {
+            console.log('Conexão fechada pelo témino da aplicação');
+            process.exit(0);
+        });
+    });
+}
